11a8abddf3acfbd5415a7563769c82fe2c905645
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-08-16 14:41:56 +0300

    Added get jobs service and search bar

--- src/components/main/JobsSection.js
+++ src/components/main/JobsSection.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import JobCard from '../job/JobCard'
+import JobService from "../../service/JobService";
+import SearchBar from "./SearchBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const JobsSection = () => {
+    const [isLoading, setIsLoading] = useState();
+    const [jobs, setJobs] = useState();
+
+    useEffect(() => {
+
+        JobService.getJobs().then(r => {
+            console.log(r.data.jobs)
+            setJobs(r.data.jobs)
+        })
+    }, [])
+
+
+
     return (
         <div>
+            <SearchBar />
             <JobCard />
         </div>
     )

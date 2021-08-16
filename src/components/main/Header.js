bd0ac4acc9ec1518f840b2b4ff45c2418f2250b3
AndreiPenica <andrei.penica@gmail.com>
2021-08-16 17:45:42 +0300

    styled Header

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -7,8 +7,14 @@ const Header = () => {
     return (
         <header>
             <NavBar />
-            <h2 className="headerText">Searching for a job?</h2>
-            <div className="headerSearchBar">SearchBar</div>
+            <h2 className="headerText">Searching for a job?<br></br>Find the <span style={{color:"rgb(30, 209, 170)", fontSize:"30px"}}>best job</span> <br></br> that fits you!</h2>
+            <div className="headerSearchBar">
+                <div></div>
+                <div></div>
+                <div>
+                    <button className="searchButton">Search Job</button>
+                </div>
+            </div>
             <img className="malePhoto" src={malePhoto} />
         </header>
     )
--- src/components/main/NavBar.js
+++ src/components/main/NavBar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import JobCard from '../job/JobCard';
+import logo from './logo.svg';
 
 const NavBar = () => {
     return (
-        <>
-            <Link to="#" className="logo">LOGO</Link>
+        <>  
+        <div className="logo">
+        <Link to="#"><img src={logo} style={{zIndex:"2", width:"50px"}} />joblish</Link>
+        {/* <Link to="#">jobify</Link> */}
+        </div>
             <ul>
                 <li><Link to="#">Jobs</Link></li>
                 <li><Link to="#">CV</Link></li>
                 <li><Link to="#">Chestie</Link></li>
+                <li><Link to="#">Join Now</Link></li>
             </ul>
 
         </>

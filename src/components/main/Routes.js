b5ea2f31333695c4180b519f540f5a1ead5c2109
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-08-16 18:15:56 +0300

    Added new fetch functionalities

--- src/components/main/Routes.js
+++ src/components/main/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import HomePage from "./HomePage";
+import JobDetails from "../job/JobDetails";
 
 const Routes = () => {
     return (
@@ -8,6 +9,7 @@ const Routes = () => {
             <Router>
                 <Switch>
                     <Route path={"/"} exact component={HomePage} />
+                    <Route path={"/job/:id"} component={JobDetails}/>
                 </Switch>
             </Router>
         </>
--- src/service/JobService.js
+++ src/service/JobService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const JOBS_API_URL = "https://remotive.io/api/remote-jobs";
-const API_KEY = "2a2e2571-8596-4969-8f0c-4e7ebd6dd4b6";
 
 
 class JobService {
@@ -11,6 +10,14 @@ class JobService {
 
     getJobsBySearchInput(searchInput) {
         return axios.get(`${JOBS_API_URL}?search=${searchInput}`)
+    }
+
+    getAllJobs() {
+        return axios.get(`${JOBS_API_URL}?limit=50`)
+    }
+
+    getJobsByCompanyName(companyName) {
+        return axios.get(`${JOBS_API_URL}?company_name=${companyName}`)
     }
 }
 

c8dab69b6a17fbff520b2eeb82d6cacaeeb7af90
Bogdan Iordan <bogdan.iordan47@yahoo.com>
2021-08-17 11:56:35 +0300

    Refactored code

--- src/components/job/JobDetails.js
+++ src/components/job/JobDetails.js
@@ -3,15 +3,21 @@ import {useLocation} from "react-router-dom";
 import NavBar from "../main/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "@material-ui/core/Button";
+import {useHistory} from "react-router-dom/cjs/react-router-dom";
 
 
 const JobDetails = () => {
     const location = useLocation();
     const job = location.state.job;
+    const history = useHistory();
 
     useEffect(() => {
         console.log(job)
     })
+
+    const apply = () => {
+        history.push(`/apply/${job.id}`)
+    }
 
     return (
         <div>
@@ -24,7 +30,7 @@ const JobDetails = () => {
             Date: {job.publication_date}
             <br/>
             Description : {job.description}
-            <Button color="primary" variant="contained">Apply</Button>
+            <Button color="primary" variant="contained" onClick={apply}>Apply</Button>
         </div>
     );
 };
--- src/components/main/Routes.js
+++ src/components/main/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import HomePage from "./HomePage";
 import JobDetails from "../job/JobDetails";
+import Application from "../application/Application";
 
 const Routes = () => {
     return (
@@ -10,6 +11,7 @@ const Routes = () => {
                 <Switch>
                     <Route path={"/"} exact component={HomePage} />
                     <Route path={"/job/:id"} component={JobDetails}/>
+                    <Route path={"/apply/;jobId"} component={Application}/>
                 </Switch>
             </Router>
         </>

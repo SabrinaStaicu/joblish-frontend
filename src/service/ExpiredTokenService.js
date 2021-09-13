9b6a61b88f8ca289f6a45415a6ed9de38af26058
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-09-13 14:00:21 +0300

    Refactored expired token service code

--- src/service/ExpiredTokenService.js
+++ src/service/ExpiredTokenService.js
@@ -2,19 +2,22 @@ import jwtDecode from "jwt-decode";
 import AuthService from "./AuthService";
 
 export default function ExpiredTokenService() {
-    let token = JSON.parse(localStorage.getItem("user")).token;
-    if (token) {
-        let decodedToken = jwtDecode(token);
-        console.log("Decoded Token", decodedToken);
-        let currentDate = new Date();
+    if (JSON.parse(localStorage.getItem("user"))) {
+        let token = JSON.parse(localStorage.getItem("user")).token;
+        if (token) {
+            let decodedToken = jwtDecode(token);
+            console.log("Decoded Token", decodedToken);
+            let currentDate = new Date();
 
-        // JWT exp is in seconds
-        if (decodedToken.exp * 1000 < currentDate.getTime()) {
-            console.log("Token expired.");
-            AuthService.logout();
-        } else {
-            console.log("Valid token");
+            // JWT exp is in seconds
+            if (decodedToken.exp * 1000 < currentDate.getTime()) {
+                console.log("Token expired.");
+                AuthService.logout();
+            } else {
+                console.log("Valid token");
+            }
         }
     }
+
 
 }
